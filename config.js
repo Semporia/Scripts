d3848f886fa1eb017a396dbedfbe0b5100fafd88
王鹏 <wangpengwnba@126.com>
2018-06-28 01:01:54 +0800

    .

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret': 'hunter',
-    'database': 'mongodb://myblog:526900@23.105.205.19:27017/myBlog'
+    'database': 'mongodb://myblog:526900@23.83.242.217:27017/myBlog'
 };
