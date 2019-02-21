7379f91a57e581aba36c438c5112aba35f96ea9c
whyour <imwhyour@gmail.com>
2019-02-21 12:05:10 +0800

    update server

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 
+const url = require('url');
+const crypto = require('crypto');
+
+
+// Web 服务器端口
+// 微信公众平台服务器配置中的 Token
+const token = '8dI6rx4iBtPqRWmEjE8h';
+
 var config = require('./config'); //读取配置文件config.js信息
 var User = require('./app/models/yiyan'); //获取 yiyan model 信息
 var port = process.env.PORT || 8080; // 设置启动端口
@@ -61,11 +69,70 @@ app.get('/yi', function (req, res) {
 });
 
 app.post('/worktile', function (req, res) {
+    checkSignature(req, res);
 
     console.log('req.body', req.body);
 
-    res.send({ code: 290 });
+    // res.send({ code: 290 });
 });
+
+function sha1(str) {
+    const md5sum = crypto.createHash('sha1');
+    md5sum.update(str);
+    const ciphertext = md5sum.digest('hex');
+    return ciphertext;
+  }
+  
+  function _decode(data) {
+    const AESKey = Buffer.from('21IpFqj8qolJbaqPqe1rVTAK5sgkaQ3GQmUKiUQLwRe', 'base64')
+    const iv = AESKey.slice(0, 16)
+    const decipher = crypto.createDecipheriv('aes-256-cbc', AESKey, iv)
+    decipher.setAutoPadding(false)
+    return decodePkcs(
+      Buffer.concat([
+        decipher.update(data, 'base64'),
+        decipher.final()
+      ])
+    )
+  }
+  
+  function decodePkcs(buf) {
+    blockSize = 32;
+    let padLen = buf[buf.length - 1]
+    if (padLen < 1 || padLen > blockSize)
+      padLen = 0
+    return buf.slice(0, buf.length - padLen)
+  }
+
+  function checkSignature(req, res) {
+    console.log('hhh');
+    
+    const query = url.parse(req.url, true).query;
+    console.log('Request URL: ', req.url);
+    const signature = query.signature;
+    const timestamp = query.timestamp;
+    const nonce = query.nonce;
+    const echostr = query.echostr;
+    console.log('timestamp: ', timestamp);
+    console.log('nonce: ', nonce);
+    console.log('signature: ', signature);
+    // 将 token/timestamp/nonce 三个参数进行字典序排序
+    const tmpArr = [token, timestamp, nonce];
+    const tmpStr = sha1(tmpArr.sort().join(''));
+    console.log('Sha1 String: ', tmpStr);
+    // 验证排序并加密后的字符串与 signature 是否相等
+    if (tmpStr === signature) {
+      // 原样返回echostr参数内容
+      const last = _decode(echostr);
+      console.log('yes');
+      
+      res.end(last);
+      console.log('Check Success');
+    } else {
+      res.end('failed');
+      console.log('Check Failed');
+    }
+  }
 
 app.listen(port);
 console.log('Magic happens at http://localhost:' + port);
