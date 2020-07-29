59b612ce8792a2bd28e66b76910966b3595bd085
whyour <imwhyour@gmail.com>
2020-07-29 17:52:45 +0800

    更新yiyan数据库字符串

--- app/models/yiyan.js
+++ app/models/yiyan.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var config = require('../../config'); //读取配置文件config.js信息
+const yiyan = mongoose.createConnection(config.database1); // 连接数据库
 
-module.exports = mongoose.model('yiYan', new Schema({
+module.exports = yiyan.model('yiYan', new Schema({
     content: String,
     from: String,
     type: String,
