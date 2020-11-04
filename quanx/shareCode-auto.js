aae75f16505b42b2c122cd598c096330944b2a63
whyour <imwhyour@gmail.com>
2020-11-04 15:35:02 +0800

    修改默认互助码

--- quanx/shareCode-auto.js
+++ quanx/shareCode-auto.js
@@ -2,8 +2,8 @@ const $ = new Env('互助码')
 const shareCodes = [
   {
     zd: $.getdata("zd_shareCode1") || "qu73szyyke7tv4sgpjojpyiq6u",
-    nc: $.getdata("nc_shareCode1") || "qu73szyyke7tv4sgpjojpyiq6u",
-    mc: $.getdata("mc_shareCode1") || "qu73szyyke7tv4sgpjojpyiq6u",
+    nc: $.getdata("nc_shareCode1") || "a1bc1602151a42d68fb8cf0ee93cc43d",
+    mc: $.getdata("mc_shareCode1") || "MTAxODc2NTEzNDAwMDAwMDAyNzkyNjM4Mw==",
   },
   {
     zd: $.getdata("zd_shareCode2") || "",
