cb5900491e5c9145fcbcfe250052bdd48d365a97
whyour <imwhyour@gmail.com>
2020-11-30 18:15:19 +0800

    添加京喜开团api

---
+++ src/services/jxFactoryTuan.ts
@@ -0,0 +1,42 @@
+import { Document } from 'mongoose';
+import { Service, Inject } from 'typedi';
+import { ICommon } from '../interfaces/ICommon';
+
+@Service()
+export default class JxFactoryService {
+  constructor(@Inject('jxFactoryTuan') private jxFactoryTuan: Models.ICommonModel, @Inject('logger') private logger) {}
+
+  public async getCode(): Promise<{ code: ICommon }> {
+    const [record] = await this.jxFactoryTuan.aggregate([{ $sample: { size: 1 } }]);
+    if (!record) {
+      throw new Error('get shiCi error');
+    }
+    return { code: record };
+  }
+
+  public async createCode({ name, code }: { name: string; code: string }): Promise<{ code: ICommon & Document }> {
+    const record = await this.jxFactoryTuan.findOneAndUpdate(
+      { name },
+      {
+        value: code,
+        name: name,
+        type: 1,
+      },
+      { upsert: true, new: true, setDefaultsOnInsert: true },
+    );
+    if (!record) {
+      throw new Error('get shiCi error');
+    }
+    return { code: record };
+  }
+
+  public async countCode(): Promise<{ count: Number }> {
+    const count = await this.jxFactoryTuan.count({});
+    return { count };
+  }
+
+  public async removeCode(): Promise<{ msg: string }> {
+    const count = await this.jxFactoryTuan.remove({});
+    return { msg: 'success' };
+  }
+}
