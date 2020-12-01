6286267c4330a636ac7f1c1d1ce15efcf679dfd0
whyour <imwhyour@gmail.com>
2020-12-01 10:48:24 +0800

    添加calendar api

--- src/api/routes/yiYan.ts
+++ src/api/routes/yiYan.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { Container } from 'typedi';
 import YiYanService from '../../services/yiYan';
 import { celebrate, Joi } from 'celebrate';
+import fetch from 'node-fetch';
 import { Logger } from 'winston';
 const route = Router();
 
@@ -13,6 +14,19 @@ export default (app: Router) => {
       const yiYanServiceInstance = Container.get(YiYanService);
       const { yiYan } = await yiYanServiceInstance.getYiYan();
       return res.status(200).json({ ...yiYan });
+    } catch (e) {
+      logger.error('🔥 error: %o', e);
+      return next(e);
+    }
+  });
+
+  route.get('/calendar', async (req: Request, res: Response, next: NextFunction) => {
+    const logger: Logger = Container.get('logger');
+    try {
+      const text = await fetch(`https://wannianrili.51240.com/ajax/?q=${req.query.q}`, {
+        method: "get",
+      }).then((res) => res.text());
+      res.status(200).send(text)
     } catch (e) {
       logger.error('🔥 error: %o', e);
       return next(e);
