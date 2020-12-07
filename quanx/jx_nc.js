ed001129f4acb5b7cf5dd437b843f7a9b1a30bfe
whyour <imwhyour@gmail.com>
2020-12-07 15:21:08 +0800

    修复京喜农场助力和种子类型通知

--- quanx/jx_nc.js
+++ quanx/jx_nc.js
@@ -4,6 +4,7 @@
  * @Date: 2020-12-06 11:11:11
  * @LastEditors: whyour
  * @LastEditTime: 2020-12-06 23:16:27
+  只能选择非京喜app专属种子
   quanx:
   [task_local]
   10 9,18 * * * https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js, enabled=true
@@ -22,6 +23,9 @@ const JD_API_HOST = 'https://wq.jd.com/';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.showLog = $.getdata('nc_showLog') ? $.getdata('nc_showLog') === 'true' : false;
 $.notifyTime = $.getdata('nc_notifyTime');
+$.openUrl = encodeURIComponent(
+  `openjd://virtual?params={ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2" }`,
+);
 $.result = [];
 $.cookieArr = [];
 $.currentCookie = '';
@@ -37,9 +41,11 @@ $.info = {};
         $.currentCookie.match(/pt_pin=(.+?);/) && $.currentCookie.match(/pt_pin=(.+?);/)[1],
       );
       $.log(`\n开始【京东账号${i + 1}】${userName}`);
-      await getTaskList();
+      const isSuccess = await getTaskList();
+      if (!isSuccess) return;
       await $.wait(500);
-      await browserTask();
+      const isOk = await browserTask();
+      if (!isOk) return;
       await $.wait(500);
       await submitInviteId(userName);
       await $.wait(500);
@@ -66,20 +72,23 @@ function getCookies() {
   return true;
 }
 
-
 function getTaskList() {
   return new Promise(async resolve => {
     $.get(taskUrl('query', `type=1`), async (err, resp, data) => {
       try {
         const res = data.match(/try\{whyour\(([\s\S]*)\)\;\}catch\(e\)\{\}/)[1];
         const { detail, msg, task = [], retmsg, ...other } = JSON.parse(res);
-        $.allTask = task.filter(x=>x.tasktype !== 3 && x.tasktype !== 2);
+        $.allTask = task.filter(x => x.tasktype !== 3 && x.tasktype !== 2 && x.tasktype !== 14);
         $.info = other;
         $.log(`\n获取任务列表 ${retmsg} 总共${$.allTask.length}个任务！`);
+        if (!$.info.active) {
+          $.msg($.name, '请先去京喜农场选择种子！', '必须选择非app专属种子，点击通知跳转', { 'open-url': $.openUrl });
+          resolve(false);
+        }
       } catch (e) {
         $.logErr(e, resp);
       } finally {
-        resolve();
+        resolve(true);
       }
     });
   });
@@ -91,20 +100,24 @@ function browserTask() {
     for (let i = 0; i < $.allTask.length; i++) {
       const task = $.allTask[i];
       $.log(`\n开始第${i + 1}个任务：${task.taskname}`);
-      const status = [true];
+      const status = [0];
       for (let i = 0; i < times; i++) {
         await $.wait(500);
-        if (status[0]) {
+        if (status[0] === 0) {
           status[0] = await doTask(task);
         }
-        await $.wait(500);
-        if (!status[0]) {
+        if (status[0] !== 0) {
           break;
         }
       }
+      if (status[0] === 1032) {
+        $.msg($.name, '请选择非京喜app专属种子！', '必须选择非app专属种子，点击通知跳转', { 'open-url': $.openUrl }););
+        resolve(false);
+        return;
+      }
       $.log(`\n结束第${i + 1}个任务：${task.taskname}\n`);
     }
-    resolve();
+    resolve(true);
   });
 }
 
@@ -115,23 +128,29 @@ function doTask({ tasklevel, left, taskname }) {
       $.log(`\n${taskname}[做任务]： 任务已完成，跳过`);
       return;
     }
-    $.get(taskUrl('dotask', `active=${$.info.active}&joinnum=${$.info.joinnum}&tasklevel=${tasklevel}`), (err, resp, data) => {
-      try {
-        const res = data.match(/try\{whyour\(([\s\S]*)\)\;\}catch\(e\)\{\}/)[1];
-        $.log(res)
-        const { ret, retmsg = '' } = JSON.parse(res);
-        $.log(
-          `\n${taskname}[做任务]：${retmsg.indexOf('活动太火爆了') !== -1 ? '任务进行中或者未到任务时间' : retmsg}${
-            $.showLog ? '\n' + res : ''
-          }`,
-        );
-        resolve(ret === 0);
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally {
-        resolve();
-      }
-    });
+    $.get(
+      taskUrl(
+        'dotask',
+        `active=${$.info.active}&answer=${$.info.indexday}:D:0&joinnum=${$.info.joinnum}&tasklevel=${tasklevel}`,
+      ),
+      (err, resp, data) => {
+        try {
+          const res = data.match(/try\{whyour\(([\s\S]*)\)\;\}catch\(e\)\{\}/)[1];
+          $.log(res);
+          const { ret, retmsg = 'success' } = JSON.parse(res);
+          $.log(
+            `\n${taskname}[做任务]：${retmsg.indexOf('活动太火爆了') !== -1 ? '任务进行中或者未到任务时间' : retmsg}${
+              $.showLog ? '\n' + res : ''
+            }`,
+          );
+          resolve(ret);
+        } catch (e) {
+          $.logErr(e, resp);
+        } finally {
+          resolve();
+        }
+      },
+    );
   });
 }
 
@@ -169,24 +188,26 @@ function createAssistUser() {
       try {
         const { data = {} } = JSON.parse(_data);
         $.log(`\n${data.value}\n${$.showLog ? _data : ''}`);
-        $.get(taskUrl('help', `active=${$.info.active}&joinnum=${$.info.joinnum}&smp=b47f4790d7b2a024e75279f55f6249b9`), (err, resp, data) => {
-          try {
-            const res = data.match(/try\{whyour\(([\s\S]*)\)\;\}catch\(e\)\{\}/)[1];
-            const { ret, retmsg = '' } = JSON.parse(res);
-            $.log(`\n助力：${retmsg} \n${$.showLog ? res : ''}`);
-          } catch (e) {
-            $.logErr(e, resp);
-          } finally {
-            resolve();
-          }
-        });
+        $.get(
+          taskUrl('help', `active=${$.info.active}&joinnum=${$.info.joinnum}&smp=${data.value}`),
+          (err, resp, data) => {
+            try {
+              const res = data.match(/try\{whyour\(([\s\S]*)\)\;\}catch\(e\)\{\}/)[1];
+              const { ret, retmsg = '' } = JSON.parse(res);
+              $.log(`\n助力：${retmsg} \n${$.showLog ? res : ''}`);
+            } catch (e) {
+              $.logErr(e, resp);
+            } finally {
+              resolve();
+            }
+          },
+        );
       } catch (e) {
         $.logErr(e, resp);
       }
     });
   });
 }
-
 
 function showMsg() {
   return new Promise(resolve => {
