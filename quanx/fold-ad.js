1cd75d8a83ff98a3e3a16a4d6038021f6f9a2773
whyour <imwhyour@gmail.com>
2021-02-18 17:57:08 +0800

    去除虎牙直播首屏广告

--- quanx/fold-ad.js
+++ quanx/fold-ad.js
@@ -1,17 +1,18 @@
 /*
  * @Author: whyour
  * @Github: https://github.com/whyour
- * @Date: 2020-12-04 18:49:09
+ * @Date: 2021-02-18 17:56:09
  * @LastEditors: whyour
  * @LastEditTime: 2020-12-04 18:53:52
  */
 
 const xiChuangZhu = '/functions/getLaunchImageForIOS';
+const huYaZhiBo = 'business.msstatic.com/advertiser/material'
 
 const url = $request.url;
 let body = $response.body;
 
-if (url.indexOf(xiChuangZhu) != -1) {
+if (url.indexOf(xiChuangZhu) != -1 || url.indexOf(huYaZhiBo) !== -1) {
   body = JSON.stringify({});
 }
 
