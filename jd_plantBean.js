9f1f4a4a1b32127b1dff2266a7bd91b2e0767fbe
he1pu <86448703+he1pu@users.noreply.github.com>
2021-06-28 20:53:33 +0800

    自动提交助力码，删除内置助力码

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -39,6 +39,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
+  ''
 ]; 
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
