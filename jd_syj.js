f3f5dc924dbcf3df66d5e76b677954ecffe67ebb
he1pu <86448703+he1pu@users.noreply.github.com>
2021-07-01 22:41:10 +0800

    Update jd_syj.js

--- jd_syj.js
+++ jd_syj.js
@@ -554,26 +554,26 @@ async function distributeBeanActivity() {
     if ($.tuan && $.tuan.hasOwnProperty('assistedPinEncrypted') && $.assistStatus !== 3) {
       $.tuanList.push($.tuan);
       const code = Object.assign($.tuan, {"time": Date.now()});
-      $.http.post({
-        url: `http://go.chiang.fun/autocommit`,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ "act": "zuan", code }),
-        timeout: 30000
-      }).then((resp) => {
-        if (resp.statusCode === 200) {
-          try {
-            let { body } = resp;
-            body = JSON.parse(body);
-            if (body['code'] === 200) {
-              console.log(`\n【京东账号${$.index}（${$.nickName || $.UserName}）的【赚京豆-瓜分京豆】好友互助码提交成功\n`)
-            } else {
-              console.log(`【赚京豆-瓜分京豆】邀请码提交失败:${JSON.stringify(body)}\n`)
-            }
-          } catch (e) {
-            console.log(`【赚京豆-瓜分京豆】邀请码提交异常:${e}`)
-          }
-        }
-      }).catch((e) => console.log(`【赚京豆-瓜分京豆】邀请码提交异常:${e}`));
+//       $.http.post({
+//         url: `http://go.chiang.fun/autocommit`,
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ "act": "zuan", code }),
+//         timeout: 30000
+//       }).then((resp) => {
+//         if (resp.statusCode === 200) {
+//           try {
+//             let { body } = resp;
+//             body = JSON.parse(body);
+//             if (body['code'] === 200) {
+//               console.log(`\n【京东账号${$.index}（${$.nickName || $.UserName}）的【赚京豆-瓜分京豆】好友互助码提交成功\n`)
+//             } else {
+//               console.log(`【赚京豆-瓜分京豆】邀请码提交失败:${JSON.stringify(body)}\n`)
+//             }
+//           } catch (e) {
+//             console.log(`【赚京豆-瓜分京豆】邀请码提交异常:${e}`)
+//           }
+//         }
+//       }).catch((e) => console.log(`【赚京豆-瓜分京豆】邀请码提交异常:${e}`));
     }
   } catch (e) {
     $.logErr(e);
