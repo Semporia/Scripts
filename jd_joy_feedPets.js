751c6806664f9eb714db1eca9eb5b74bf51bd5bf
he1pu <86448703+he1pu@users.noreply.github.com>
2021-08-10 22:46:35 +0800

    Update jd_joy_feedPets.js

--- jd_joy_feedPets.js
+++ jd_joy_feedPets.js
@@ -21,7 +21,7 @@ cron "15 0-23/1 * * *" script-path=jd_joy_feedPets.js,tag=京东宠汪汪喂食
 京东宠汪汪喂食 = type=cron,script-path=jd_joy_feedPets.js, cronexpr="15 0-23/1 * * *", timeout=3600, enable=true
 */
 const $ = new Env('宠汪汪🐕喂食');
-const zooFaker = require('./utils/JDJRValidator_Pure');
+const zooFaker = require('./JDJRValidator_Pure');
 $.get = zooFaker.injectToRequest2($.get.bind($));
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
