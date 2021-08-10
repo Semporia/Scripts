7ce201f0bc9f9157c0dffbdfe0f374a98d2b7913
he1pu <86448703+he1pu@users.noreply.github.com>
2021-08-10 19:36:51 +0800

    Update JDJRValidator_Pure.js

--- JDJRValidator_Pure.js
+++ JDJRValidator_Pure.js
@@ -9,7 +9,8 @@ const stream = require('stream');
 const zlib = require('zlib');
 const vm = require('vm');
 const PNG = require('png-js');
-const UA = require('./USER_AGENTS.js').USER_AGENT;
+let UA = require('./USER_AGENTS.js').USER_AGENT;
+const validatorCount = process.env.JDJR_validator_Count ? process.env.JDJR_validator_Count : 100
 
 
 Math.avg = function average() {
