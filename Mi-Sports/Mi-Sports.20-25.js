359f50d8d7f2af981b4786ff58f01a8341c569f2
Semporia <Swanpor@gmail.com>
2021-08-10 08:12:10 +0800

    Generated Files On 2021-08-10 08:12:10

--- Mi-Sports/Mi-Sports.20-25.js
+++ Mi-Sports/Mi-Sports.20-25.js
@@ -37,7 +37,7 @@ const headers = {
 }
 let login_token = '';
 //需要修改的运动步数波动范围，脚本默认修改步数范围为1w9到2w5
-const step = randomFriendPin($.getdata('xmMinStep') * 1 || 22000, $.getdata('xmMaxStep') * 1 || 24000);
+const step = randomFriendPin($.getdata('xmMinStep') * 1 || 20000, $.getdata('xmMaxStep') * 1 || 25000);
 
 function getToken() {
   if ($response.body) {
