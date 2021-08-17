5f60e9a9035027ca08140ee6e0b654cbba11a7e9
he1pu <86448703+he1pu@users.noreply.github.com>
2021-08-17 13:33:36 +0800

    修复扫码

--- getJDCookie.js
+++ getJDCookie.js
@@ -1,6 +1,6 @@
 /**
  * 扫码获取京东cookie，此方式得到的cookie有效期为30天
- * Modify from FanchangWang https://github.com/FanchangWang
+ * Modify from FanchangWang https://github.com/FanchangWang 
  */
 const $ = new Env('扫码获取京东cookie');
 // const JD_UA = require('./USER_AGENTS').USER_AGENT;
