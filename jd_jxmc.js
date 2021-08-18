6d74b8f436dffd2f2a488d6d3ff4dc16ed8e1256
he1pu <86448703+he1pu@users.noreply.github.com>
2021-08-18 19:50:02 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -198,7 +198,7 @@ async function pasture() {
         //割草
         console.log(`\n开始进行割草`);
         $.runFlag = true;
-        for (let i = 0; i < 30 && $.runFlag; i++) {
+        for (let i = 0; i < 10 && $.runFlag; i++) {
           $.mowingInfo = {};
           console.log(`开始第${i + 1}次割草`);
           await takeGetRequest('mowing');
@@ -214,7 +214,7 @@ async function pasture() {
         //横扫鸡腿
         $.runFlag = true;
         console.log(`\n开始进行横扫鸡腿`);
-        for (let i = 0; i < 30 && $.runFlag; i++) {
+        for (let i = 0; i < 10 && $.runFlag; i++) {
           console.log(`开始第${i + 1}次横扫鸡腿`);
           await takeGetRequest('jump');
           await $.wait(2000);
