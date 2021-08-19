16b34afa5ba5e208b85d1c979421db04cc5c1a86
he1pu <86448703+he1pu@users.noreply.github.com>
2021-08-19 15:49:18 +0800

    自动提交助力码，删除内置助力码

--- jd_health.js
+++ jd_health.js
@@ -286,7 +286,7 @@ function readShareCode() {
         resolve(data || {"code":500});
       }
     })
-    //await $.wait(10000);
+    await $.wait(10000);
     resolve({"code":500})
   })
 }
@@ -310,7 +310,7 @@ function submitCode() {
         resolve(data || {"code":500});
       }
     })
-    //await $.wait(15000);
+    await $.wait(10000);
     resolve({"code":500})
   })
 }
