e23ac2096634be992fd9d114040defecd839a33d
he1pu <86448703+he1pu@users.noreply.github.com>
2021-08-19 15:42:19 +0800

    自动提交助力码，删除内置助力码

--- jd_818.js
+++ jd_818.js
@@ -769,7 +769,7 @@ function submitCode(myInviteCode, userName) {
         resolve(data || {"code":500});
       }
     })
-    //await $.wait(15000);
+    await $.wait(10000);
     resolve({"code":500})
   })
 }
@@ -801,7 +801,7 @@ function readShareCode() {
         resolve(data || {"code":500});
       }
     })
-    //await $.wait(2000);
+    await $.wait(10000);
     resolve({"code":500})
   })
 }
