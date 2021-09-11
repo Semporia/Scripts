61deb27d831ff2440d5afa718792b29342c17d7f
he1pu <86448703+he1pu@users.noreply.github.com>
2021-09-11 22:23:32 +0800

    Update jd_cfd_mooncake.js

--- jd_cfd_mooncake.js
+++ jd_cfd_mooncake.js
@@ -672,7 +672,7 @@ function shareCodesFormat() {
     // if (readShareCodeRes && readShareCodeRes.code === 200) {
     //   $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
     // }
-    console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
+    //console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
 }
