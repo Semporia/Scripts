6f075af6e200bd879098cf3944082ee489c530bc
he1pu <86448703+he1pu@users.noreply.github.com>
2021-09-15 21:31:39 +0800

    Update jd_jxSign.js

--- jd_jxSign.js
+++ jd_jxSign.js
@@ -186,7 +186,7 @@ function signhb(type = 1) {
                   'num': helpNum || 0,
                   'max': max
                 })
-                if ($.index==cookiesArr.length) $.shareCodes.push({'use': 'zero205','smp': '60a620f96e505406586b8e249eda3ad8','num': 0,'max': false})
+                if ($.index==cookiesArr.length) $.shareCodes.push({'use': 'jdU','smp': '60a620f96e505406586b8e249eda3ad8','num': 0,'max': false})
               }
               break
             case 2:
