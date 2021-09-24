2c3ae631acb072c01d3022ba15b4b6327dfa3912
he1pu <86448703+he1pu@users.noreply.github.com>
2021-09-24 18:00:05 +0800

    Update jd_update.js

--- jd_update.js
+++ jd_update.js
@@ -5,16 +5,18 @@
 
 const exec = require('child_process').exec;
 
-exec("cd /ql/repo/JDHelp_jd_scripts; git fetch --all; git reset --hard origin/main; git pull", (error, stdout, stderr) => {
-  console.log(1, error)
-  console.log(2, stdout.trim())
-  console.log(3, stderr)
+if(process.env.PWD==='/ql/scripts'){
+  exec("cd /ql/repo/he1pu_JDHelp")
+}
+
+exec("git fetch --all; git reset --hard origin/main; git pull", (error, stdout, stderr) => {
+  console.log(stdout.trim())
 })
 
-if (__dirname.indexOf('/ql/') > -1) {
-  exec('ql repo https://github.com/he1pu/JDHelp.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon" "^jd[^_]|USER|utils"', (error, stdout, stderr) => {
-    console.log(1, error)
-    console.log(2, stdout.trim())
-    console.log(3, stderr)
-  })
+if(process.env.PWD==='/ql/scripts') {
+  if (__dirname.indexOf('/ql/') > -1) {
+    exec('ql repo https://github.com/he1pu/JDHelp.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon" "^jd[^_]|USER|utils"', (error, stdout, stderr) => {
+      console.log(stdout.trim())
+    })
+  }
 }
