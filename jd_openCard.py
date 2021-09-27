09d20c3103fbafb1aea59e710a6ac04355d09d83
he1pu <86448703+he1pu@users.noreply.github.com>
2021-09-27 16:51:51 +0800

    Update jd_openCard.py

--- jd_openCard.py
+++ jd_openCard.py
@@ -6,7 +6,8 @@ Author: Curtin
 功能：JD入会开卡领取京豆
 CreateDate: 2021/5/4 下午1:47
 UpdateTime: 2021/6/19
-建议cron: 2 8,15 * * * jd_OpenCard.py, tag=开卡有礼, enabled=true
+建议cron: 
+2 8,15 * * * jd_OpenCard.py, tag=开卡有礼, enabled=true
 new Env('开卡有礼');
 '''
 version = 'v1.2.2'
