9d9fcff4e9799a1ae3729a7f0f63c3ad9324bdbd
he1pu <86448703+he1pu@users.noreply.github.com>
2021-10-08 15:57:24 +0800

    Update jd_opencardSVIP.js

--- jd_opencardSVIP.js
+++ jd_opencardSVIP.js
@@ -15,14 +15,7 @@
 [task_local]
 #10.8~10.14 超店会员福利社
 47 0,20 8-14 10 * https://raw.githubusercontent.com/he1pu/JDHelp/main/jd_opencardSVIP.js, tag=10.8~10.14 超店会员福利社, enabled=true
-================Loon==============
-[Script]
-cron "47 0,20 8-14 10 *" script-path=https://raw.githubusercontent.com/he1pu/JDHelp/main/jd_opencardSVIP1.js,tag=10.8~10.14 超店会员福利社
-===============Surge=================
-10.8~10.14 超店会员福利社 = type=cron,cronexp="47 0,20 8-14 10 *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/he1pu/JDHelp/main/jd_opencardSVIP.js
-============小火箭=========
-10.8~10.14 超店会员福利社 = type=cron,script-path=https://raw.githubusercontent.com/he1pu/JDHelp/main/jd_opencardSVIP.js, cronexpr="47 0,20 8-14 10 *", timeout=3600, enable=true
-***/
+*/
 
 const $ = new Env('10.8~10.14 超店会员福利社');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
