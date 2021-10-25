e1a8e0492a0a4a3675ed80841c1319163a4079c2
he1pu <86448703+he1pu@users.noreply.github.com>
2021-10-25 15:22:44 +0800

    Update jd_hyj.js

--- jd_hyj.js
+++ jd_hyj.js
@@ -513,7 +513,7 @@ async function toHelp() {
         let res = await help(inviteIds[j])
         if (res && res['data'] && res.code == 0) {
             console.log(`助力: ${res.data.bizMsg}`)
-            if (res.data?.bizCode == -6 || res.data?.bizCode == 108) {
+            if (res.data?.bizCode == -6 || res.data?.bizCode == 108 || res.data.bizMsg == '啊哦，活动太火爆了~') {
                 //console.log(`助力次数已耗尽，跳出`)
                 break
             }
@@ -561,7 +561,7 @@ async function toHelpPk() {
         let res = await helpPk($.pkInviteId);
         if (res && res.code == 0 && res.data) {
           console.log(`组队：${res.data.bizMsg}`)
-          if (res.data.bizCode == -5) {
+          if (res.data.bizCode == -5 || res.data.bizMsg == '啊哦，活动太火爆了~') {
             break;
           }
         }else {
@@ -574,7 +574,7 @@ async function toHelpPk() {
         let res = await helpPk(pkPool[j]);
         if (res && res.code == 0 && res.data) {
           console.log(`组队：${res.data.bizMsg}`)
-          if (res.data.bizCode == -5) {
+          if (res.data.bizCode == -5 || res.data.bizMsg == '啊哦，活动太火爆了~') {
             break;
           }
         }else {
