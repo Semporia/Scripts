8f7ae0ab89013e47cf52ddd5bf21a71771b3bf63
he1pu <86448703+he1pu@users.noreply.github.com>
2022-01-04 10:24:00 +0800

    Update jd_bean_change_pro.js

--- jd_bean_change_pro.js
+++ jd_bean_change_pro.js
@@ -213,9 +213,9 @@ async function bean() {
 function TotalBean() {
     return new Promise(async resolve => {
         const options = {
-            url: "https://wq.jd.com/user_new/info/GetJDUserInfoUnion?sceneval=2",
+            url: "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion",
             headers: {
-                Host: "wq.jd.com",
+                Host: "me-api.jd.com",
                 Accept: "*/*",
                 Connection: "keep-alive",
                 Cookie: cookie,
