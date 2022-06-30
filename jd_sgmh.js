6e0d0def168f399b2cfe62b81261017896bd28bb
he1pu <86448703+he1pu@users.noreply.github.com>
2022-06-30 10:03:06 +0800

    Update jd_sgmh.js

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -321,7 +321,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://www.helpu.cf/jdcodes/getcode.php?type=sgmh&num=${randomCount}`,
+      url: `http://www.jdhelp.cf/jdcodes/getcode.php?type=sgmh&num=${randomCount}`,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
@@ -347,7 +347,7 @@ function readShareCode() {
 //提交互助码
 function submitCode() {
     return new Promise(async resolve => {
-    $.get({url: `http://www.helpu.cf/jdcodes/submit.php?code=${myInviteCode}&type=sgmh`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `http://www.jdhelp.cf/jdcodes/submit.php?code=${myInviteCode}&type=sgmh`, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
