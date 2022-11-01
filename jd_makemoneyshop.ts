cc5a28efa879bb4a020cc1bcce1e80e6ec75ec35
JDHelloWorld <jerfy286@qq.com>
2022-11-01 22:34:26 +0800

    guest help

--- jd_makemoneyshop.ts
+++ jd_makemoneyshop.ts
@@ -85,8 +85,11 @@ class Jd_makemoneyshop extends JDHelloWorld {
       console.log('助力码', res.data.shareId)
       this.shareCodeSelf.push(res.data.shareId)
 
+      // res = await this.task('prmt_exchange/client/exchange', {'ruleId': '1848d61655f979f8eac0dd36235586ba'})
+      // this.o2s(res, 'exchange 0.3')
+      // return
+
       res = await this.task('GetUserTaskStatusList', {})
-      this.o2s(res)
       for (let t of res.data.userTaskStatusList) {
         if ([3538, 3539].includes(t.taskId) && t.awardStatus === 2) {
           if (t.completedTimes !== t.configTargetTimes) {
@@ -105,6 +108,7 @@ class Jd_makemoneyshop extends JDHelloWorld {
   }
 
   async help(users: User[]) {
+    let res: any, shareCode: string[] = [], shareCodeHW: string[] = []
     for (let user of users) {
       this.user = user
       if (this.black.includes(this.user.UserName)) {
@@ -112,7 +116,8 @@ class Jd_makemoneyshop extends JDHelloWorld {
         continue
       }
       this.user.UserAgent = `jdltapp;iPhone;4.2.2;Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)`
-      let res: any, shareCode: string[] = [], shareCodeHW: string[] = []
+      this.h5stTool = new H5ST('d06f1', this.user.UserAgent, this.fp, 'https://wqs.jd.com/sns/202210/20/make-money-shop/index.html', 'https://wqs.jd.com', this.user.UserName)
+      await this.h5stTool.__genAlgo()
       this.o2s(this.shareCodeSelf, '内部助力')
 
       if (shareCodeHW.length === 0) {
@@ -127,7 +132,12 @@ class Jd_makemoneyshop extends JDHelloWorld {
         for (let code of shareCode) {
           console.log(`账号${user.index + 1} ${user.UserName} 去助力 ${code}`)
           res = await this.api('querysharevenderinfo', 'activeId,shareId', {activeId: '63526d8f5fe613a6adb48f03', shareId: code})
-          console.log('助力结果', res.data.guestInfo.guestErrMsg)
+          console.log('获取信息', res.data.guestInfo.guestErrMsg)
+          if (res.data.guestInfo.guestErrMsg === '天助力次数限制') {
+            break
+          }
+          res = await this.api('guesthelp', 'activeId,shareId', {activeId: '63526d8f5fe613a6adb48f03', shareId: code})
+          this.o2s(res)
           await this.wait(2000)
         }
       } catch (e) {
