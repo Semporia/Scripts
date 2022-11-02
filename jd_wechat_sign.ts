4158a00f7b8e879a67549d15c3425fdcfd2f2e88
JDHelloWorld <jerfy286@qq.com>
2022-11-02 21:01:56 +0800

    jd_wechat_sign.ts 0.25🧧

--- jd_wechat_sign.ts
+++ jd_wechat_sign.ts
@@ -4,18 +4,25 @@
  * cron: 8 0 * * *
  */
 
-import {H5ST} from "./utils/h5st_3.1"
+import {H5ST} from "./utils/h5st_pro"
 import {User, JDHelloWorld} from "./TS_JDHelloWorld";
 
 class Jd_wechat_sign extends JDHelloWorld {
   user: User
   h5stTool: H5ST
+  fp: string
 
   constructor() {
     super("微信签到");
   }
 
   async init() {
+    try {
+      this.fp = process.env.FP_9A38A ?? await this.getFp()
+    } catch (e) {
+      console.log('FP Error: ', e.message)
+      process.exit(0)
+    }
     await this.run(this)
   }
 
@@ -24,13 +31,13 @@ class Jd_wechat_sign extends JDHelloWorld {
       'appid': 'hot_channel',
       'body': JSON.stringify(body),
       'client': 'apple',
-      'clientVersion': '7.21.190',
+      'clientVersion': '7.22.240',
       'functionId': `SignComponent_${fn}`,
     })
 
     let temp: string = fn !== 'startScanTask' ? 'signTask' : 'scanTask'
     let fnId: string = fn !== 'startScanTask' ? fn : 'doScanTask'
-    return this.post(`https://api.m.jd.com/${temp}/${fn}`, `client=apple&clientVersion=7.21.190&functionId=SignComponent_${fnId}&appid=hot_channel&loginType=2&body=${encodeURIComponent(JSON.stringify(body))}&h5st=${h5st}`, {
+    return this.post(`https://api.m.jd.com/${temp}/${fn}`, `client=apple&clientVersion=7.22.240&functionId=SignComponent_${fnId}&appid=hot_channel&loginType=2&body=${encodeURIComponent(JSON.stringify(body))}&h5st=${h5st}`, {
       'Host': 'api.m.jd.com',
       'wqreferer': 'http://wq.jd.com/wxapp/pages/market/market2/index',
       'referer': 'https://servicewechat.com/wx91d27dbf599dff74/656/page-frame.html',
@@ -39,33 +46,43 @@ class Jd_wechat_sign extends JDHelloWorld {
     })
   }
 
+  async task(fn: string, body: object, signComponent: string) {
+    let h5st: string = await this.h5stTool.__genH5st({
+      appid: "hot_channel",
+      body: JSON.stringify(body),
+      client: "android",
+      clientVersion: "7.22.240",
+      functionId: signComponent,
+    })
+    return await this.post(`https://api.m.jd.com/scanTask/${fn}`, `client=android&clientVersion=7.22.240&functionId=${signComponent}&appid=hot_channel&body=${encodeURIComponent(JSON.stringify(body))}&h5st=${h5st}`, {
+      'Host': 'api.m.jd.com',
+      'wqreferer': 'http://wq.jd.com/wxapp/pages/market/market2/index',
+      'User-Agent': this.user.UserAgent,
+      'Referer': 'https://servicewechat.com/wx91d27dbf599dff74/664/page-frame.html',
+      'Cookie': this.user.cookie
+    })
+  }
+
   async main(user: User) {
     try {
       this.user = user
       this.user.UserAgent = `Mozilla/5.0 (iPhone; CPU iPhone OS ${this.getIosVer()} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.28(0x18001c2b) NetType/WIFI Language/zh_CN`
       let res: any
+      // this.h5stTool = new H5ST("9a38a", this.user.UserAgent, this.fp, 'http://wq.jd.com/wxapp/pages/market/market2/index', 'http://wq.jd.com', this.user.UserName);
+      // await this.h5stTool.__genAlgo()
+      // res = await this.api('doSignTask', {"activityId": "10004", "version": 1})
+      // this.o2s(res, 'doSignTask')
 
-      this.h5stTool = new H5ST("9a38a", this.user.UserAgent, process.env.FP_9A38A, 'http://wq.jd.com/wxapp/pages/market/market2/index', 'http://wq.jd.com', this.user.UserName);
+      this.h5stTool = new H5ST("2b5bc", this.user.UserAgent, this.fp, 'http://wq.jd.com/wxapp/pages/market/market2/index', 'http://wq.jd.com', this.user.UserName);
       await this.h5stTool.__genAlgo()
-
-      res = await this.api('querySignStatus', {"activityId": "10004", "activeId": "", "groupId": "", "version": 1})
-      this.o2s(res, 'querySignStatus')
-
-      res = await this.api('doSignTask', {"activityId": "10004", "version": 1})
-      this.o2s(res, 'doSignTask')
-
-      res = await this.api('querySignList', {"activityId": "10004", "version": 1})
-      this.o2s(res, 'querySignList')
-
+      res = await this.task('querySignList', {"activityId": "10004", "version": 1}, 'SignComponent_querySignList')
       if (!res.data.scanTaskInfo.completionFlag) {
-        res = await this.api('startScanTask', {"itemId": res.data.scanTaskInfo.itemId, "activityId": "10004", "scanAssignmentId": res.data.scanTaskInfo.scanAssignmentId, "actionType": 1, "version": 1})
-        this.o2s(res, 'startScanTask 1')
-        await this.wait(10000)
-
-        res = await this.api('startScanTask', {"itemId": res.data.scanTaskInfo.itemId, "activityId": "10004", "scanAssignmentId": res.data.scanTaskInfo.scanAssignmentId, "actionType": 0, "version": 1})
-        this.o2s(res, 'startScanTask 0')
+        res = await this.task('startScanTask', {"itemId": res.data.scanTaskInfo.itemId, "activityId": "10004", "scanAssignmentId": res.data.scanTaskInfo.scanAssignmentId, "actionType": 1, "version": 1}, 'SignComponent_doScanTask')
+        console.log('开始任务', res.message || res.success)
+        await this.wait(8000)
+        res = await this.task('startScanTask', {"activityId": "10004", "actionType": 0, "scanAssignmentId": res.data.scanTaskInfo.scanAssignmentId, "itemId": res.data.scanTaskInfo.itemId, "version": 1}, 'SignComponent_doScanTask')
+        console.log('领取奖励', res.data.rewardList[0].discount)
       }
-
     } catch (e) {
       console.log(e.message)
     }
